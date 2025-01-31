8922d02453b30138eea0bd6eb131b8aae5747741
faizshaikh17 <mohdfaiz8101@gmail.com>
2025-02-01 01:54:33 +0530

    reduxTodo done

--- redux/src/features/todoSlice.js
+++ redux/src/features/todoSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = {
-    todos: [{ id: 1, text: "Try me!!" }]
+    todos: [{ id: 1, text: "Do the necessary!!" }]
 }
 
 export const todoSlice = createSlice({
