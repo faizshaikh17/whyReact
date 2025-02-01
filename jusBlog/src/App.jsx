9e37059983d8e1a78121d18677df70560483e569
faizshaikh17 <mohdfaiz8101@gmail.com>
2025-02-02 04:43:02 +0530

    getUser service initiated

--- jusBlog/src/App.jsx
+++ jusBlog/src/App.jsx
@@ -1,12 +1,38 @@
 import './App.css'
-import conf from './conf/conf'
-
+import { Header } from './components/Header/Header'
+import { Footer } from './components/Footer/Footer'
+import { useEffect, useState } from 'react'
+import authService from './appwrite/auth'
+import { useDispatch } from 'react-redux'
+import { login, logout } from './features/authSlice'
 function App() {
-  return (
+
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService.getUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      }).finally(setLoading(false))
+  }, [loading])
+
+
+
+  return !loading ? (
     <>
-      <h1>jusBlog</h1>
+      <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+        <div className='w-full block'>
+          <Header />
+          {/* <Outlet /> */}
+          <Footer />
+        </div>
+      </div>
     </>
-  )
+  ) : null
 }
 
 export default App
