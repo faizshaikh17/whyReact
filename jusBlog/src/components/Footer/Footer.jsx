e6094dc57645ab529817d5f16699c0b2dc51c100
faizshaikh17 <mohdfaiz8101@gmail.com>
2025-02-03 00:57:41 +0530

    SignUp & Login added

--- jusBlog/src/components/Footer/Footer.jsx
+++ jusBlog/src/components/Footer/Footer.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
 
-export const Footer = () => {
+import React from "react";
+import githubLogo from "../../assets/img/github.png";
+import { FaGithub } from "react-icons/fa";
+
+export function Footer() {
     return (
-        <div>Footer</div>
-    )
+        <footer className="bg-background dark:bg-dark-bg text-primary dark:text-dark-primary px-6 py-4  bottom-0 left-0 w-full">
+            <div className="flex justify-center items-center space-x-4 flex-col gap-2">
+                <p className="text-center text-sm">© BlogApp Made by Faiz Shaikh</p>
+                <a
+                    href="https://github.com/faizshaikh17/whyReact/tree/main/jusBlog"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <div className="flex gap-3">
+                        <span className="text-sm">Repo Link : </span>
+                        <FaGithub className="h-5 w-5" />
+                    </div>
+                </a>
+            </div>
+        </footer>
+    );
 }
+
+export default Footer;
--- jusBlog/src/components/Header/Header.jsx
+++ jusBlog/src/components/Header/Header.jsx
@@ -1,7 +1,61 @@
 import React from 'react'
+import { Container, Logo, LogoutBtn } from '../index'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+
 
 export const Header = () => {
+
+  const authStatus = useSelector((state) => state.auth.status)
+  const navigate = useNavigate()
+
+  navItem = [
+    {
+      name: "Home",
+      slug: "/",
+      active: true
+    },
+    {
+      name: "SignUp",
+      slug: "/signup",
+      active: !authStatus
+    },
+    {
+      name: "Login",
+      slug: "/login",
+      active: !authStatus
+    },
+    {
+      name: "Add Post",
+      slug: "/add-post",
+      active: authStatus
+    },
+  ]
+
   return (
-    <div>Header</div>
+    <header className='py-3 shadow bg-gray-500'>
+      <Container>
+        <nav className='flex'>
+          <div className='mr-4'>
+            <link to='/'>
+              <Logo />
+            </link >
+          </div>
+          <ul className='flex ml-auto'>
+            {navItem.map((item) =>
+              item.active ? (
+                <li key={item.name}>
+                  <button className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full' onClick={() => navigate(item.slug)}>{item.name}</button>
+                </li>
+              ) : null
+            )}
+            {authStatus && (
+              <li><LogoutBtn /></li>
+            )}
+          </ul>
+        </nav>
+      </Container>
+    </header>
   )
 }
